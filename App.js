2947d9f09343fa6aa485bb1feecaf2e8c8d04c20
Chibu <chibueze.k.muoneke@gmail.com>
2024-05-25 00:26:06 +0100

    finished, code is self explanatory fr

--- App.js
+++ App.js
@@ -1,16 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Switch, Text, View } from 'react-native';
 import "react-native-gesture-handler";
+import { BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { useRef, useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
+  const bottomModalRef = useRef(null);
+
+  const [darkMode, setDarkMode] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const snapPoints = ['25%', '48%', '70%']; // How much of the screen we want the sheet to take
+
+  function handlePresentModal() {
+    bottomModalRef.current?.present();
+    setTimeout(() => {
+      setIsOpen(true);
+    }, 150);
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <View style={[styles.container, { backgroundColor: isOpen ? 'gray' : 'white' }]}> 
+          <Button title='Present Modal' onPress={handlePresentModal} />
+          <StatusBar style="auto" />
+          <BottomSheetModal
+            ref={bottomModalRef}
+            index={1}
+            snapPoints={snapPoints}
+            backgroundStyle={{ borderRadius: 30 }}
+            onDismiss={() => setIsOpen(false)}
+          >
+            <View style={styles.contentContainer}>
+              <Text style={styles.title}>Hello World</Text>
 
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-    
+              <View style={styles.row}>
+                <Text>Dark Mode</Text>
+                <Switch value={darkMode} onValueChange={() => setDarkMode(!darkMode)} />
+              </View>
+
+            </View>
+          </BottomSheetModal>
+        </View>
+      </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
 }
@@ -18,8 +52,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  contentContainer:{
+    flex:1,
+    alignItems:'center',
+    paddingHorizontal:15,
+  },
+  title:{
+    fontWeight: '900',
+    fontSize:16,
+    letterSpacing:0.5,
+    marginBottom:20
+  },
+
+  row:{
+    width:'100%',
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+  },
+
+  subtitle:{
+    color:'#101318',
+    fontSize:14,
+    fontWeight: 'bold'
+  }
 });
